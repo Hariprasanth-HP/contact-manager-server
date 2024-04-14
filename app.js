81b8b4a2f45d3e4490532ef4544bb355e2e29764
Hari Prasanth <hari16205@gmail.com>
2024-04-14 12:57:59 +0530

    primary route

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const bodyParser = require("body-parser");
 app.use(cors({}));
 app.use(bodyParser.json());
 app.use(router);
+app.get("/", (req, res) => {
+  res.json({ data: [{ name: "hari", age: "1" }] });
+});
 app.listen(5000, () => {
   console.log("App is listening on port 5000");
 });
